3c116cb57818ae5f27995c37af28f23d44b22dd8
Gustaxbjj <153648794+TalysG26@users.noreply.github.com>
2025-09-14 14:27:59 -0300

    Parte superior

---
+++ src/components/ui/Menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu(){7
+    return(
+  <div className="space-y-1.5 cursor-pointer">
+  <span className="block w-8 h-1 bg-amber-50"></span>
+  <span className="block w-8 h-1 bg-amber-50"></span>
+  <span className="block w-8 h-1 bg-amber-50"></span>
+</div>
+    )
+}
---
+++ src/components/ui/NameUser.tsx
@@ -0,0 +1,8 @@
+export default function NameUser(){
+    return(
+        <div className="p-10">
+      <span className="text-5xl"> Olá,</span>
+      <span className="text-5xl font-bold"> Talys!</span>
+    </div>
+    )
+}

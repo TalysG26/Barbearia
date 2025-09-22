3088aab5cd7743b14f2b58460769af8cbeb73a6c
Gustaxbjj <153648794+TalysG26@users.noreply.github.com>
2025-09-22 12:02:35 -0300

    vai

--- src/components/ui/Acabamento.tsx
+++ src/components/ui/Acabamento.tsx
@@ -1,6 +1,6 @@
 export default function Cabelo() {
   return (
-   <div className="p-4 w-fit border border-gray-400 flex items-center gap-2 rounded-lg bg-darkbg">
+   <div className="p-4 w-fit border border-gray-400 flex items-center gap-2 rounded-lg bg-darkbg    ">
   <img src="/acabamento.svg" alt="Cabelo" className="w-20 h-10" />
   <div className="text-xl font-medium text-white">Acabamento</div>
 </div>
